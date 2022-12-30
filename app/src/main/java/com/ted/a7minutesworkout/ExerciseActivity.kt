f106afaba195524d320926d3a0b4fb57ff640100
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-29 23:23:50 -0800

    Adding The Exercise Screen and Customizing our Toolbar

--- app/src/main/java/com/ted/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/ted/a7minutesworkout/ExerciseActivity.kt
@@ -2,11 +2,24 @@ package com.ted.a7minutesworkout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ted.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 
 }
