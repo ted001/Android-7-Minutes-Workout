30c1ace3afc0c5af968dbe9ed28738cab450b797
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-30 17:26:13 -0800

    Preparing the BMI Screen

--- app/src/main/java/com/ted/a7minutesworkout/BMIActivity.kt
+++ app/src/main/java/com/ted/a7minutesworkout/BMIActivity.kt
@@ -2,10 +2,20 @@ package com.ted.a7minutesworkout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ted.a7minutesworkout.databinding.ActivityBmiBinding
 
 class BMIActivity : AppCompatActivity() {
+    private var binding: ActivityBmiBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+        binding = ActivityBmiBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
+        setSupportActionBar(binding?.toolbarBmiActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //set back button
+        supportActionBar?.title = "CALCULATE BMI" // Setting a title in the action bar.
+        binding?.toolbarBmiActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
 }
