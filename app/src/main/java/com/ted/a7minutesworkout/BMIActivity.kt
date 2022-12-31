e47cd478af68a7a9d2151bc802e936e98ee21812
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-30 19:06:38 -0800

    Adding Custom RadioGroups with Custom RadioButtons

--- app/src/main/java/com/ted/a7minutesworkout/BMIActivity.kt
+++ app/src/main/java/com/ted/a7minutesworkout/BMIActivity.kt
@@ -66,30 +66,39 @@ class BMIActivity : AppCompatActivity() {
         val bmiLabel: String
         val bmiDescription: String
 
-        if (bmi.compareTo(15f) <= 0) {
-            bmiLabel = "Very severely underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
-        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0) {
-            bmiLabel = "Severely underweight"
-            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
-        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0) {
-            bmiLabel = "Underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
-        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0) {
-            bmiLabel = "Normal"
-            bmiDescription = "Congratulations! You are in a good shape!"
-        } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(bmi, 30f) <= 0) {
-            bmiLabel = "Overweight"
-            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
-        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0) {
-            bmiLabel = "Obese Class | (Moderately obese)"
-            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
-        } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0) {
-            bmiLabel = "Obese Class || (Severely obese)"
-            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
-        } else {
-            bmiLabel = "Obese Class ||| (Very Severely obese)"
-            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        when {
+            bmi.compareTo(15f) <= 0 -> {
+                bmiLabel = "Very severely underweight"
+                bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+            }
+            bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0 -> {
+                bmiLabel = "Severely underweight"
+                bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+            }
+            bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0 -> {
+                bmiLabel = "Underweight"
+                bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+            }
+            bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0 -> {
+                bmiLabel = "Normal"
+                bmiDescription = "Congratulations! You are in a good shape!"
+            }
+            bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <= 0 -> {
+                bmiLabel = "Overweight"
+                bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+            }
+            bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0 -> {
+                bmiLabel = "Obese Class | (Moderately obese)"
+                bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+            }
+            bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0 -> {
+                bmiLabel = "Obese Class || (Severely obese)"
+                bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+            }
+            else -> {
+                bmiLabel = "Obese Class ||| (Very Severely obese)"
+                bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+            }
         }
 
         //Use to set the result layout visible
