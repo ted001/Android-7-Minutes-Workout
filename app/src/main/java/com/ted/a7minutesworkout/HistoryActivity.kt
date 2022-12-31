606504be5dff2f0920931bad1979048135402ac6
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-30 19:27:05 -0800

    Adding the History UI and Activity

--- app/src/main/java/com/ted/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/ted/a7minutesworkout/HistoryActivity.kt
@@ -2,10 +2,31 @@ package com.ted.a7minutesworkout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ted.a7minutesworkout.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
+    private var binding: ActivityHistoryBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarHistoryActivity)
+
+        val actionbar = supportActionBar//actionbar
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true) //set back button
+            actionbar.title = "HISTORY" // Setting a title in the action bar.
+        }
+
+        binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
 }
